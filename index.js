996de23cc622e7f0ac2d370441bc88ef287ec009
Patricio Bottos <patobottos@gmail.com>
2023-01-25 12:38:38 +0100

    Nivell 2 - Exercicis 4 i 5

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 // EXERCICI 4
 window.addEventListener("load", () => {
-  let weatherIcon = document.querySelector(".weather-img");
-  let cityName = document.querySelector(".city");
-  let temperatureValue = document.querySelector(".weather-temperature");
-  let description = document.querySelector(".weather-description");
+  const weatherIcon = document.querySelector(".weather-img");
+  const cityName = document.querySelector(".city");
+  const temperatureValue = document.querySelector(".weather-temperature");
+  const description = document.querySelector(".weather-description");
 
   const urlWeather = `https://api.weatherapi.com/v1/current.json?key=a2df7834642248acbd3153507232401&q=Barcelona&aqi=no`;
 
@@ -34,6 +34,7 @@ window.addEventListener("load", () => {
       console.log(error);
     }
   };
+
   getWeather();
 });
 
@@ -43,7 +44,8 @@ const nextJoke = document.getElementById("fetched-new-joke");
 const btnNextJoke = document.getElementById("btn-next-joke");
 const btnSubmitVote = document.getElementById("btn-submitVote");
 const reportJokes = [];
-const fetchedJoke = "";
+const fetchedJoke1 = "";
+const fetchedJoke2 = "";
 
 const urlJokes1 = "https://icanhazdadjoke.com/";
 const options1 = {
@@ -56,44 +58,60 @@ const urlJokes2 = "https://api.chucknorris.io/jokes/random";
 
 // JS FUNCTIONALITY
 
-// SHOW SCORE BUTTONS
+/// SHOW SCORE BUTTONS
 const showScoreButtons = () => {
   document.getElementById("score-btn-container").style.display = "block";
 };
 
-//HIDE SCORE BUTTONS
+/// HIDE SCORE BUTTONS
 const hideScoreButtons = () => {
   document.getElementById("score-btn-container").style.display = "none";
   document.getElementById("fetched-new-joke").style.display = "none";
 };
 
-// RETRIEVE A JOKE
+/// RETRIEVE A DAD JOKE
 const getJoke1 = async () => {
   try {
-    const askingNewJoke = await fetch(urlJokes1, options1);
-    const data = await askingNewJoke.json();
-    const fetchedJoke = data.joke;
+    const askingNewJoke1 = await fetch(urlJokes1, options1);
+    const dataDad = await askingNewJoke1.json();
+    const fetchedJoke1 = dataDad.joke;
 
-    nextJoke.innerHTML = fetchedJoke;
-    console.log(fetchedJoke);
+    nextJoke.innerHTML = fetchedJoke1;
+    console.log(`The random joke is =>`,fetchedJoke1);
     showScoreButtons();
+
   } catch (err) {
     console.log(err);
   }
 };
 
-// FUNCTION NEXT JOKE
-btnNextJoke.addEventListener("click", () => {
-  console.log("You've pressed the button!");
-  getJoke1();
-});
+/// RETRIEVE CHUCK NORRIS JOKE 
+const getJoke2 = async () => {
+  try {
+    const askingNewJoke2 = await fetch(urlJokes2);
+    const fetchedJoke2 = await askingNewJoke2.json();
 
-//FUNCTION DETECT VOTE
+    nextJoke.innerHTML = fetchedJoke2.value;
+    console.log(`The random joke is =>`,fetchedJoke2.value);
+    showScoreButtons();
+
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+/// FUNCTION TO CHOOSE API
+const randomSelector = () => {
+  return Math.floor(Math.random() * 2);
+};
+
+/// FUNCTION DETECT VOTE
 const detectVote = () => {
   const allOptions = document.getElementsByName("joke-score");
-  const selectedValue = Array.from(allOptions).find((radio) => radio.checked);
-  //console.log('score =>', selectedValue.value);
+  //console.log('allOptions',allOptions);
 
+  const selectedValue = Array.from(allOptions).find((radio) => radio.checked);
+  
   let printedJoke = document.getElementById("fetched-new-joke").innerHTML;
   //console.log('fetched joke => ',printedJoke);
 
@@ -105,10 +123,34 @@ const detectVote = () => {
   //console.log('obj joke: ', reviewedJoke);
 
   reportJokes.push(reviewedJoke);
+  console.table(reportJokes);
+
+  allOptions.forEach(option => option.checked = false);
 };
+
+/// FUNCTION NEXT JOKE
+const askForNextJoke = () => {
+  console.log("You've pressed the button!");
+  let choice = randomSelector();
+  choice === 1 ? getJoke1() : getJoke2();
+  detectVote();
+}
+
+
+// ESBORRAR AL FINAL
+
+/*
 
 // FUNCTION SCORE JOKE
 btnSubmitVote.addEventListener("click", () => {
   detectVote();
   console.table(reportJokes);
 });
+
+
+/// RESET SELECTED OPTIONS 
+const resetRadios = () => {
+  allOptions.forEach(option => option.value = null );
+}
+
+*/
